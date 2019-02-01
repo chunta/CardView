4c0f352714a63ee65f19567272e4ad1fe7dacde8
Rex <rex.chen@appledaily.com.tw>
2019-02-01 17:56:29 +0800

    prepare

---
+++ RCardView/RCardViewCell/RCardColViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RCardColViewCell.swift
+//  RCardView
+//
+//  Created by nmi on 2019/2/1.
+//  Copyright © 2019 nmi. All rights reserved.
+//
+
+import UIKit
+
+class RCardColViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
