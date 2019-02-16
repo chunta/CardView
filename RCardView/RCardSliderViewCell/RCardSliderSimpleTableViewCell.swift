4b57698ff6130a6f44a64f19eaa9842421a647d0
Rex <rex.chen@appledaily.com.tw>
2019-02-16 18:47:17 +0800

    Shot

---
+++ RCardView/RCardSliderViewCell/RCardSliderSimpleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RCardSliderSimpleTableViewCell.swift
+//  RCardView
+//
+//  Created by nmi on 2019/2/15.
+//  Copyright © 2019 nmi. All rights reserved.
+//
+
+import UIKit
+
+class RCardSliderSimpleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
